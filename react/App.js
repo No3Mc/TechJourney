fd7a7bf32fe301b9140b339aa8058f81719037a4
thr33 <number3mcpe@gmail.com>
2024-03-01 13:17:02 +0000

    react: Glasses

--- react/App.js
+++ react/App.js
@@ -7,18 +7,26 @@ class App extends React.Component {
     }
 
     toggleGlasses = () => {
-        this.setState({ showGlasses: true });
+        this.setState(prevState => ({ showGlasses: !prevState.showGlasses }));
     }
 
     render() {
         return (
             <div className="container">
-                {!this.state.showGlasses && (
-                    <button onClick={this.toggleGlasses}>Show Glasses</button>
-                )}
-                {this.state.showGlasses && (
-                    <div className="glasses"></div>
-                )}
+                <div className="button-container">
+                    <button onClick={this.toggleGlasses}>
+                        {this.state.showGlasses ? "Hide Glasses" : "Show Glasses"}
+                    </button>
+                </div>
+                <div className="glasses-container"
+                     style={{ "--glasses-top-margin": "30%", "--glasses-top-offset": "0", "--glasses-bottom-offset": "0" }}>
+                    {this.state.showGlasses && (
+                        <div className="glasses">
+                            <div className="lens left"></div>
+                            <div className="lens right"></div>
+                        </div>
+                    )}
+                </div>
             </div>
         );
     }
