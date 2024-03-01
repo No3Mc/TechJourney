86a75afb7d6be3e1dfc20f261ea5c904d31a20d0
thr33 <number3mcpe@gmail.com>
2024-03-01 12:35:15 +0000

    react: Testing glasses

--- react/App.js
+++ react/App.js
@@ -2,21 +2,23 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            wearingGlasses: false
+            showGlasses: false
         };
     }
 
     toggleGlasses = () => {
-        this.setState(prevState => ({
-            wearingGlasses: !prevState.wearingGlasses
-        }));
+        this.setState({ showGlasses: true });
     }
 
     render() {
         return (
             <div className="container">
-                <button onClick={this.toggleGlasses}>Toggle Glasses</button>
-                <div className={`person ${this.state.wearingGlasses ? 'with-glasses' : ''}`}></div>
+                {!this.state.showGlasses && (
+                    <button onClick={this.toggleGlasses}>Show Glasses</button>
+                )}
+                {this.state.showGlasses && (
+                    <div className="glasses"></div>
+                )}
             </div>
         );
     }
