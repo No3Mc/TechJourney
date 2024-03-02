5966e5762e0a965618a2c2da26d3fca95b44d2ab
thr33 <number3mcpe@gmail.com>
2024-03-02 14:46:41 +0000

    Script

--- script.js
+++ script.js
@@ -26,3 +26,41 @@
 //   });
 // }
 
+        // Toggle theme
+        const themeToggle = document.getElementById("theme-toggle");
+        const sunIcon = document.getElementById("sun-icon");
+        const moonIcon = document.getElementById("moon-icon");
+        let isNightMode = true;
+    
+        themeToggle.addEventListener("mouseenter", () => {
+            if (isNightMode) {
+                sunIcon.style.display = "inline-block";
+                moonIcon.style.display = "none";
+            } else {
+                sunIcon.style.display = "none";
+                moonIcon.style.display = "inline-block";
+            }
+        });
+    
+        themeToggle.addEventListener("mouseleave", () => {
+            if (isNightMode) {
+                sunIcon.style.display = "none";
+                moonIcon.style.display = "inline-block";
+            } else {
+                sunIcon.style.display = "inline-block";
+                moonIcon.style.display = "none";
+            }
+        });
+    
+        themeToggle.addEventListener("click", () => {
+            const themeStyle = document.getElementById("theme-style");
+            if (themeStyle.getAttribute("href") === "lm.css") {
+                themeStyle.setAttribute("href", "nm.css");
+                document.body.classList.add("night-mode");
+                isNightMode = true;
+            } else {
+                themeStyle.setAttribute("href", "lm.css");
+                document.body.classList.remove("night-mode");
+                isNightMode = false;
+            }
+        });
