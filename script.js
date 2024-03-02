459fed5bb118e1eb6923b7488aec799afe912aed
thr33 <number3mcpe@gmail.com>
2024-03-02 14:55:29 +0000

    A bit neat maybe

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-// import { getDocument } from 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.0.269/pdf.mjs';
-
-// pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.0.269/pdf.worker.mjs';
-
-// getDocument('https://no3mc.com/cv.pdf').promise.then(pdfDoc_ => {
-//   pdfDoc_.getPage(1).then(page => {
-//     renderPage(page, 'pdf-canvas-1');
-//   });
-
-//   pdfDoc_.getPage(2).then(page => {
-//     renderPage(page, 'pdf-canvas-2');
-//   });
-// });
-
-// function renderPage(page, canvasId) {
-//   var scale = 1.5;
-//   var viewport = page.getViewport({ scale: scale });
-//   var canvas = document.getElementById(canvasId);
-//   var context = canvas.getContext('2d');
-//   canvas.height = viewport.height;
-//   canvas.width = viewport.width;
-
-//   var renderTask = page.render({ canvasContext: context, viewport: viewport });
-//   renderTask.promise.then(() => {
-//     console.log('Page rendered', canvasId);
-//   });
-// }
-
         // Toggle theme
         const themeToggle = document.getElementById("theme-toggle");
         const sunIcon = document.getElementById("sun-icon");
@@ -54,12 +26,12 @@
     
         themeToggle.addEventListener("click", () => {
             const themeStyle = document.getElementById("theme-style");
-            if (themeStyle.getAttribute("href") === "lm.css") {
-                themeStyle.setAttribute("href", "nm.css");
+            if (themeStyle.getAttribute("href") === "theme/lm.css") {
+                themeStyle.setAttribute("href", "theme/nm.css");
                 document.body.classList.add("night-mode");
                 isNightMode = true;
             } else {
-                themeStyle.setAttribute("href", "lm.css");
+                themeStyle.setAttribute("href", "theme/lm.css");
                 document.body.classList.remove("night-mode");
                 isNightMode = false;
             }
