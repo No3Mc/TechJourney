5f5074e1de8368eb3a5516c0bca5cf30404f2ef9
thr33 <number3mcpe@gmail.com>
2024-03-12 15:04:50 +0000

    projects/projects.: Adding project's details

---
+++ projects/projects.js
@@ -0,0 +1,38 @@
+        // Toggle theme
+        const themeToggle = document.getElementById("theme-toggle");
+        const sunIcon = document.getElementById("sun-icon");
+        const moonIcon = document.getElementById("moon-icon");
+        let isNightMode = true;
+    
+        themeToggle.addEventListener("mouseenter", () => {
+            if (isNightMode) {
+                sunIcon.style.display = "inline-block";
+                moonIcon.style.display = "none";
+            } else {
+                sunIcon.style.display = "none";
+                moonIcon.style.display = "inline-block";
+            }
+        });
+    
+        themeToggle.addEventListener("mouseleave", () => {
+            if (isNightMode) {
+                sunIcon.style.display = "none";
+                moonIcon.style.display = "inline-block";
+            } else {
+                sunIcon.style.display = "inline-block";
+                moonIcon.style.display = "none";
+            }
+        });
+    
+        themeToggle.addEventListener("click", () => {
+            const themeStyle = document.getElementById("theme-style");
+            if (themeStyle.getAttribute("href") === "../theme/lm.css") {
+                themeStyle.setAttribute("href", "../theme/nm.css");
+                document.body.classList.add("night-mode");
+                isNightMode = true;
+            } else {
+                themeStyle.setAttribute("href", "../theme/lm.css");
+                document.body.classList.remove("night-mode");
+                isNightMode = false;
+            }
+        });
